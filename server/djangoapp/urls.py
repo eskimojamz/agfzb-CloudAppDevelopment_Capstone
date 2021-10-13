12c1accb958b53d41d7dcf54ff784e0a7826dd8c
eskimojamz <jameskim917@gmail.com>
2021-10-13 12:57:30 +0000

    first

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,21 +8,21 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
-    # path for about view
-
-    # path for contact us view
-
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
+    
     path(route='', view=views.get_dealerships, name='index'),
 
-    # path for dealer reviews view
+    path(route='about/', view=views.about, name='about'),
 
-    # path for add a review view
+    path(route='contact/', view=views.contact, name='contact'),
+
+    path('registration/', views.registration_request, name='registration'),
+
+    path('login/', views.login_request, name='login'),
+
+    path('logout/', views.logout_request, name='logout'),
+
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+
+    path('review/<int:dealer_id>/', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
